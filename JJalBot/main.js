1dbc03195a623cdaac80d8f70fe04bb2ad2604d7
simhana <daum_c0m@naver.com>
2024-07-11 19:55:34 +0900

    require

---
+++ JJalBot/main.js
@@ -0,0 +1,5 @@
+const res = require("./print.js")
+
+console.log(res.sum(5, 10));
+console.log(res.divide(4, 2));
+console.log(res.multiply(8, 12));
---
+++ JJalBot/print.js
@@ -0,0 +1,12 @@
+exports.sum = function(a, b){
+    return a+b;
+}
+
+exports.divide = function(a, b){
+    return a-b;
+}
+
+exports.multiply = function(a, b){
+    return a*b;
+}
+
