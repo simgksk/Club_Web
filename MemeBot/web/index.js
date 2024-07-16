0512b0b2f9315f4468185be4d290d046f66e36de
simhana <daum_c0m@naver.com>
2024-07-16 20:27:57 +0900

    login

--- MemeBot/web/index.js
+++ MemeBot/web/index.js
@@ -5,9 +5,11 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/", function(req, res){
-    res.render("index", {title: "안녕하세요", name: "we"});
-});
+app.use(express.static(path.join(__dirname, "static")));
+
+app.get("/login", function(req, res){
+    res.render("login", {});
+})
 
 app.listen(3000, () => {
     console.log("3000포트에서 서버 실행됨");
